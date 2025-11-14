bae04ee1fc1e4624c4658b29f428c5d8f988e86e
kenny459 <kennedyuyanwanne@gmail.com>
2025-11-14 23:14:25 +0100

    Remove unused import

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import SearchBar from '../components/SearchBar'
 
-import MovieCard from '../components/MovieCard' 
 
 
 import TabButtons from '../components/TabButtons'
